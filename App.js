c4347cca776293adf1dd89b706231df414200238
Shoaib Khan <shb.khan@gmail.com>
2024-10-09 18:14:02 -0400

    Update

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, Text, Button } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
+// Works on iOS
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+    console.log(selectedDate);
+  };
+
+  useEffect(() => {
+    setShow(true);
+    setMode("date");
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Date: {date.toISOString().substring(0, 10)}</Text>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            onChange={onChange}
+            style={{ backgroundColor: "pink" }}
+          />
+        )}
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+// Works on Android
+// export default function App() {
+//   const [date, setDate] = useState(new Date());
+
+//   const onChange = (event, selectedDate) => {
+//     const currentDate = selectedDate;
+//     setDate(currentDate);
+//   };
+
+//   const showMode = (currentMode) => {
+//     DateTimePickerAndroid.open({
+//       value: date,
+//       onChange,
+//       mode: currentMode,
+//       is24Hour: true,
+//     });
+//   };
+
+//   const showDatepicker = () => {
+//     showMode("date");
+//   };
+
+//   const showTimepicker = () => {
+//     showMode("time");
+//   };
+
+//   return (
+//     <SafeAreaProvider>
+//       <SafeAreaView style={styles.container}>
+//         <Button onPress={showDatepicker} title="Date" />
+//         <Button onPress={showTimepicker} title="Time" />
+//         <Text>selected: {date.toLocaleString()}</Text>
+//       </SafeAreaView>
+//     </SafeAreaProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
